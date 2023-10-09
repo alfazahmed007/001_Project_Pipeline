149b56153cc2d4a023f5258cdcabe6bbc8eb8ef9
alfazahmed007 <136940212+alfazahmed007@users.noreply.github.com>
2023-10-09 23:59:36 +0530

    Update App.java

--- src/main/java/com/neuralink/App.java
+++ src/main/java/com/neuralink/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "This is the first CI-CD Pipeline by Alfaaz!" );
     }
 }
